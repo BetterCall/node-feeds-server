aec1c0ca9c3f46a367713ef0d4d3b44d5e484bef
DrSaul <bryann@brovia.com>
2017-10-11 11:05:07 +0200

    test

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function sendNotification(userId) {
 		var followersId = Object.keys(snapshot.val());
 		// foreach follower create notification
 		followersId.forEach(function(followerId) {
-			notificationRef.child(followerId).push(snapshot) 
+			notificationRef.child(followerId).push(snapshot.val()) 
 		});
 			
 	}); 
